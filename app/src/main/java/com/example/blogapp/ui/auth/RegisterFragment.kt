84016363da18ce67d2de96db31a71f6a28393b5e
Ruben Yebran <RubenYebran@gmail.com>
2021-05-09 17:36:49 -0400

    Register Fragment

--- app/src/main/java/com/example/blogapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/auth/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.example.blogapp.ui.auth
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.blogapp.R
 import com.example.blogapp.databinding.FragmentRegisterBinding
@@ -13,5 +14,44 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegisterBinding.bind(view)
+
+        binding.btnSignUp.setOnClickListener {
+            checkSignUpInfo()
+        }
+    }
+
+    private fun checkSignUpInfo() {
+
+        val email = binding.etEmail.text.toString().trim()
+        val username = binding.etUsername.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+        val confirmPassword = binding.etConfirmPassword.text.toString().trim()
+        var estado = "ok"
+
+        if (email.isEmpty() || username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            estado = "empty"
+        }else if(password!= confirmPassword ){
+            estado = "password"
+        }
+
+        when (estado) {
+            "ok" -> Toast.makeText(
+                requireContext(),
+                "All good.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            "password" -> Toast.makeText(
+                requireContext(),
+                "Error: Passwords don't match.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            "empty" -> Toast.makeText(
+                requireContext(),
+                "Error: Empty field.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }

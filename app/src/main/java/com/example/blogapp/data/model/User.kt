6c0300481c399ee0eebfc968e54c3f0daf88e76a
Ruben Yebran <RubenYebran@gmail.com>
2021-05-10 20:05:24 -0400

    Base App

--- app/src/main/java/com/example/blogapp/data/model/User.kt
+++ app/src/main/java/com/example/blogapp/data/model/User.kt
@@ -1,7 +1,12 @@
 package com.example.blogapp.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class User(
+    @SerializedName("email")
     val email: String = "",
+    @SerializedName("username")
     val username: String = "",
+    @SerializedName("photo_url")
     val photo_url: String = ""
 )

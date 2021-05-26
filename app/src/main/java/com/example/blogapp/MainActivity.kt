8f35ca8eb6e5b5e74d8d7da0f23deb5bf4b3adf2
Ruben Yebran <RubenYebran@gmail.com>
2021-05-26 19:48:02 -0400

    Menu Changes

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.blogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.blogapp.core.hide
+import com.example.blogapp.core.show
 import com.example.blogapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +21,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        observerDestinationChange()
+    }
+
+    private fun observerDestinationChange(){
+        navController.addOnDestinationChangedListener {controller, destination, arguments ->
+            when(destination.id){
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+
+                }
+
+                R.id.registerFragment ->{
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 }

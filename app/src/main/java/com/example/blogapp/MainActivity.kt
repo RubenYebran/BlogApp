1854edf0b601ec3e6bfdc423d95568801900bcf0
Ruben Yebran <RubenYebran@gmail.com>
2021-06-13 15:01:19 -0400

    Base App

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -38,6 +38,10 @@ class MainActivity : AppCompatActivity() {
                     binding.bottomNavigationView.hide()
                 }
 
+                R.id.setupProfileFragment ->{
+                    binding.bottomNavigationView.hide()
+                }
+
                 else -> {
                     binding.bottomNavigationView.show()
                 }

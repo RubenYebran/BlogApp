9fde121b38936bdf6895f0b9849da0980374fa3d
Ruben Yebran <RubenYebran@gmail.com>
2021-06-13 22:06:22 -0400

    Camera ViewModel

---
+++ app/src/main/java/com/example/blogapp/presentation/camera/CameraViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.blogapp.presentation.camera
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.blogapp.core.Result
+import kotlinx.coroutines.Dispatchers
+
+class CameraViewModel(private val repo: CameraRepo): ViewModel() {
+    fun uploadPhoto(imageBitmap: Bitmap, description:String) = liveData(Dispatchers.IO){
+        emit(Result.Loading())
+        try{
+
+        }catch(e:Exception){
+            emit(Result.Failure(e))
+        }
+    }
+}
